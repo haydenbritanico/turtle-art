e872f683e9c3a0b6a263a7556901a7f49757d385
Hayden Britanico <hayden.britanico@gmail.com>
2024-11-26 12:09:03 -0700

    Add files via upload

---
+++ art practice.py
@@ -0,0 +1,37 @@
+#instantiate into at least two different turtles: creating two different turtle identities
+
+
+pizza = {"toppings":["mushroom", "pepperoni", "sausage"],
+             "size":["small", "medium", "large"],
+             "crust":["cheesy", "regular"]}
+import turtle
+
+def irregular_pizza_bot(color):
+
+    import turtle
+
+
+#using turtle this will be a drawing that 
+joao = turtle.Turtle()
+joao.begin_fill()
+joao.right(45)
+joao.forward(100)
+joao.right(45)
+joao.forward(100)
+joao.right(90)
+joao.forward(50)
+joao.right(45)
+joao.forward(100)
+joao.right(45)
+joao.forward(100)
+joao.right(90)
+joao.forward(50)
+joao.fillcolor('yellow')
+joao.end_fill()
+
+
+
+
+freddy = turtle.Turtle()
+
+
